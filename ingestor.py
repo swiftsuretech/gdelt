17be11cf73a90c981e3fc2f6fe87e7b23e971211
Dave Whitehouse <dave@davewhitehouse.co.uk>
2020-02-07 21:50:41 +0000

    Initial Commit

---
+++ ingestor.py
@@ -0,0 +1,12 @@
+import csv
+from elasticsearch import helpers, Elasticsearch
+
+
+def csv_reader(file_name):
+    es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+    with open(file_name, 'r') as outfile:
+        reader = csv.DictReader(outfile, delimiter='\t')
+        helpers.bulk(es, reader, index="test_gdelt")
+
+
+csv_reader("/Users/dswhitehouse/PycharmProjects/gdelt/test.CSV")
